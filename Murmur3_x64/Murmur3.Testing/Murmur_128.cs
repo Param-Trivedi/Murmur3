7db6f7da92928044788c3ff6246aee44063abaac
Param Trivedi <ptrivedi@adsworks.com>
2018-07-02 13:32:59 -0400

    -- New test methods

--- Murmur3_x64/Murmur3.Testing/Murmur_128.cs
+++ Murmur3_x64/Murmur3.Testing/Murmur_128.cs
@@ -9,19 +9,32 @@ namespace Murmur3.Testing
 {
     public class Murmur_128
     {
-        
+        Murmur3_x64_128 mHash = null;
+        string strHash = "Hello World";
 
-        [Fact(DisplayName = "Test_128(x64)")]
-        public void Test_Murmur3_128_x64()
+        [Fact(DisplayName = "Test_128(x64) Seed => 0")]
+        public void Test_Murmur3_128_x64_0()
         {
-            Murmur3_x64_128 mHash = new Murmur3_x64_128(0);
-            string strHash = "Hello World";
+            mHash = new Murmur3_x64_128(0);
             byte[] finHash = mHash.ComputeHash(Encoding.ASCII.GetBytes(strHash));
 
             //Covert bytes to Hex
             string finHexHash = ByteArrayToString(finHash);
 
             Assert.Equal("db c2 a0 c1 ab 26 63 1a 27 b4 c0 9f cf 1f e6 83 ", finHexHash);
+
+        }
+
+        [Fact(DisplayName = "Test_128(x64) Seed => 123")]
+        public void Test_Murmur3_128_x64_123()
+        {
+            mHash = new Murmur3_x64_128(123);
+            byte[] finHash = mHash.ComputeHash(Encoding.ASCII.GetBytes(strHash));
+
+            //Covert bytes to Hex
+            string finHexHash = ByteArrayToString(finHash);
+
+            Assert.Equal("60 98 45 3b e1 ba 5b 6d 5e 2d b1 da 3a 18 30 51 ", finHexHash);
 
         }
 
